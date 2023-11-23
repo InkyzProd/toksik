8eb5f43e896560363da20fb0d61b0d556a5368a0
Inkyz <63397756+InkyzProd@users.noreply.github.com>
2023-11-23 16:12:51 +0700

    Add files via upload

---
+++ toxic.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+function loadToxic() {
+  try {
+    const wordsJson = fs.readFileSync('./words.json', 'utf8');
+    return JSON.parse(wordsJson).words;
+  } catch (error) {
+    console.error('Error loading words:', error);
+    return [];
+  }
+}
+
+function isToxic(message) {
+  const words = loadToxic();
+  const regex = new RegExp('\\b(' + words.join('|') + ')\\b', 'i');
+  return regex.test(message);
+}
+
+module.exports = { isToxic };
